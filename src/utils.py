59d821bb11b9f66f0fddf84335148e6a2dde9f6f
Lev Azaretov <azaretov123@gmail.com>
2025-08-30 14:58:14 +0300

    fix after 2nd (2)

--- src/utils.py
+++ src/utils.py
@@ -5,12 +5,9 @@ from exceptions import ParserFindTagException
 
 
 def get_response(session, url):
-    try:
-        response = session.get(url)
-        response.encoding = 'utf-8'
-        return response
-    except RequestException:
-        raise
+    response = session.get(url)
+    response.encoding = 'utf-8'
+    return response
 
 
 def find_tag(soup, tag, attrs=None):
